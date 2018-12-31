472d08602012d0774910905c586ae223ecb4e12a
Christopher Toth <q.alpha@gmail.com>
2018-12-31 04:30:18 -0700

    Use the gmcp support to feed room and player script variables. Now we have player.name, player.race, player.gender, player.hp, player.max_hp, etc. And lots of room stuff. And they are all driven by the MUD!

--- worlds/starmourn/scripts.lua
+++ worlds/starmourn/scripts.lua
@@ -1,4 +1,9 @@
+require "json"
+
 room = {}
+
+player = {}
+
 
 function handle_communications(name, line, wc)
   AddToHistory(wc[1], line)
@@ -20,7 +25,7 @@ function handle_GMCP(name, line, wc)
   local command = wc[1]
   local args = wc[2]
   local handler_func_name = "handle_" .. command:lower():gsub("%.", "_")
-  local handler_func = _G[handle_func_name]
+  local handler_func = _G[handler_func_name]
   if handler_func == nil then
     Note("No handler " .. handler_func_name .. " for " .. command .. " " .. args)
   else
@@ -30,6 +35,19 @@ end -- function
 
 function handle_room_info(data)
   room = data
+end
+
+function handle_char_vitals(data)
+  player.hp = data.hp
+  player.max_hp = data.maxhp
+end -- function
+
+function handle_char_status (data)
+  player.name = data.first_name
+  player.race = data.race
+  player.gender = data.gender
+  player.marks = data.marks
+  player.target = data.target
 end
 
 function ExecuteNoStack(cmd)
